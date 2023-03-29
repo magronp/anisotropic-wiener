43bb2282b6f592ede578773113d06400911d703c
Paul Magron <paul.magron@inria.fr>
2023-03-29 10:58:34 +0200

    up

--- functions/get_nsongs.m
+++ functions/get_nsongs.m
@@ -1,6 +1,6 @@
 function Nsongs = get_nsongs(data_split)
 
-Nsongs = 50
+Nsongs = 50;
 switch data_split
     case 'Dev'
         dataNaN=[1 18 29 38 49];
@@ -8,7 +8,7 @@ switch data_split
         dataNaN=[6 34 36 40];
 end
 
-Nsongs = Nsongs - length(dataNaN)
+Nsongs = Nsongs - length(dataNaN);
 
 end
 
